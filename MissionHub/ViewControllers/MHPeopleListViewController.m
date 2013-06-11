04c5a4a4393cd6a51ec49e07f5b813d3f8644873
Amarisa Robison <Amarisa@Amarisas-MacBook-Pro.local>
2013-06-11 16:03:30 -0600

    Customized UITableViewCell in storyboard and created Person object both for MHPeopleListView

--- MissionHub/ViewControllers/MHPeopleListViewController.m
+++ MissionHub/ViewControllers/MHPeopleListViewController.m
@@ -8,11 +8,33 @@
 
 #import "MHPeopleListViewController.h"
 
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@property (nonatomic, strong) NSString *name; // name of Person
+@property (nonatomic, strong) NSString *gender; // Person gender
+@property (nonatomic, strong) NSString *imageFile; // image filename of Person
+
+@end
+
+
+@implementation Person
+
+@synthesize name;
+@synthesize gender;
+@synthesize imageFile;
+
+@end
+
+
 @interface MHPeopleListViewController ()
 
 @end
 
-@implementation MHPeopleListViewController
+@implementation MHPeopleListViewController{
+    NSArray *persons;
+}
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -46,6 +68,20 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    Person *person1 = [Person new];
+    person1.name = @"Ann Anderson";
+    person1.gender = @"Female";
+    person1.imageFile = @"anderson-ann.jpg";
+
+    Person *person2 = [Person new];
+    person2.name = @"Ann Anderson";
+    person2.gender = @"Female";
+    person2.imageFile = @"anderson-ann.jpg";
+    
+    persons = [NSArray arrayWithObjects:person1, person2, nil];
+
+
+
 }
 
 - (IBAction)revealMenu:(id)sender {
@@ -60,14 +96,14 @@
 {
 #warning Potentially incomplete method implementation.
     // Return the number of sections.
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
 #warning Incomplete method implementation.
     // Return the number of rows in the section.
-    return 0;
+    return persons.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -77,7 +113,25 @@
     
     // Configure the cell...
     
+    // Configure the cell...
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    //Display recipe in the table cell
+    Person *person = [persons objectAtIndex:indexPath.row];
+    UIImageView *personImageView = (UIImageView *)[cell viewWithTag:100];
+    personImageView.image = [UIImage imageNamed:person.imageFile];
+    
+    UILabel *personNameLabel = (UILabel *)[cell viewWithTag:101];
+    personNameLabel.text = person.name;
+    
+    UILabel *personGenderLabel = (UILabel *)[cell viewWithTag:102];
+    personGenderLabel.text = person.gender;
+    
+    
     return cell;
+    
 }
 
 #pragma mark - Table view delegate
