d7e80b62dc887c54cdd433bf8c320b0d38b7c4cc
michaelharro <michael.harrison@keynote.org>
2013-06-29 00:04:03 -0600

    Added missing images.

--- MissionHub/Models/MHPerson.m
+++ MissionHub/Models/MHPerson.m
@@ -43,4 +43,60 @@
 @dynamic updatedInteractions;
 @dynamic leaderInOrganization;
 
+-(void)setRelationshipsObject:(id)relationshipObject forFieldName:(NSString *)fieldName {
+	
+	if ([fieldName isEqualToString:@"labels"]) {
+		
+		NSArray *arrayOfObjects = relationshipObject;
+		
+		[arrayOfObjects enumerateObjectsUsingBlock:^(id object, NSUInteger index, BOOL *stop) {
+			
+			MHLabel *newObject = [MHLabel newObjectFromFields:object];
+			
+			[self addLabelsObject:newObject];
+			
+		}];
+		
+	} else if ([fieldName isEqualToString:@"admins"]) {
+		
+		NSArray *arrayOfObjects = relationshipObject;
+		
+		[arrayOfObjects enumerateObjectsUsingBlock:^(id object, NSUInteger index, BOOL *stop) {
+			
+			MHPerson *newObject = [MHPerson newObjectFromFields:object];
+			
+			[self addAdminsObject:newObject];
+			[self addPeopleObject:newObject];
+			
+		}];
+		
+	} else if ([fieldName isEqualToString:@"leaders"]) {
+		
+		NSArray *arrayOfObjects = relationshipObject;
+		
+		[arrayOfObjects enumerateObjectsUsingBlock:^(id object, NSUInteger index, BOOL *stop) {
+			
+			MHPerson *newObject = [MHPerson newObjectFromFields:object];
+			
+			[self addLeadersObject:newObject];
+			[self addPeopleObject:newObject];
+			
+		}];
+		
+	} else if ([fieldName isEqualToString:@"surveys"]) {
+		
+		NSArray *arrayOfObjects = relationshipObject;
+		
+		[arrayOfObjects enumerateObjectsUsingBlock:^(id object, NSUInteger index, BOOL *stop) {
+			
+			MHSurvey *newObject = [MHSurvey newObjectFromFields:object];
+			
+			[self addSurveysObject:newObject];
+			
+		}];
+		
+	}
+	
+}
+
 @end
