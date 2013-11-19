f71ff79ceb8bfddbaece8546e3b99550c596be9f
michaelharro <michael.harrison@keynote.org>
2013-11-19 16:29:25 -0500

    updated activities so that they update the list of admins and users when successful.

--- MissionHub/Custom UI/Activities/MHDeleteActivity.m
+++ MissionHub/Custom UI/Activities/MHDeleteActivity.m
@@ -95,6 +95,24 @@ NSString * const MHActivityTypeDelete	= @"com.missionhub.mhactivity.type.delete"
 							  
 							  [[MHAPI sharedInstance] bulkDeletePeople:weakSelf.peopleToDelete withSuccessBlock:^(NSArray *result, MHRequestOptions *options) {
 								  
+								  [weakSelf.peopleToDelete enumerateObjectsUsingBlock:^(MHPerson *person, NSUInteger index, BOOL *stop) {
+									  
+									  if ([person.permissionLevel.permission_id isEqualToNumber:[MHPermissionLevel admin].remoteID]) {
+										  
+										  MHPerson *personObjectInAdminSet	= (MHPerson *)[[MHAPI sharedInstance].currentOrganization.admins findWithRemoteID:person.remoteID];
+										  [[MHAPI sharedInstance].currentOrganization removeAdminsObject:personObjectInAdminSet];
+										  
+									  }
+									  
+									  if ([person.permissionLevel.permission_id isEqualToNumber:[MHPermissionLevel user].remoteID]) {
+										  
+										  MHPerson *personObjectInAdminSet	= (MHPerson *)[[MHAPI sharedInstance].currentOrganization.users findWithRemoteID:person.remoteID];
+										  [[MHAPI sharedInstance].currentOrganization removeUsersObject:personObjectInAdminSet];
+										  
+									  }
+									  
+								  }];
+								  
 								  SIAlertView *successAlertView = [[SIAlertView alloc] initWithTitle:@"Success"
 																				   andMessage:[NSString stringWithFormat:@"%d people were successfully deleted?", weakSelf.peopleToDelete.count]];
 								  [successAlertView addButtonWithTitle:@"Ok"
