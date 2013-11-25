78a78f03e5564e771c0f2110bfc3566c2703a197
Harro <michael.harrison@cru.org>
2013-11-25 16:06:42 -0500

    added empty files for direction activity

---
+++ MissionHub/Custom UI/Activities/MHDirectionsActivity.h
@@ -0,0 +1,13 @@
+//
+//  MHDirectionsActivity.h
+//  MissionHub
+//
+//  Created by Michael Harrison on 11/25/13.
+//  Copyright (c) 2013 Cru. All rights reserved.
+//
+
+#import "MHActivity.h"
+
+@interface MHDirectionsActivity : MHActivity
+
+@end
---
+++ MissionHub/Custom UI/Activities/MHDirectionsActivity.m
@@ -0,0 +1,13 @@
+//
+//  MHDirectionsActivity.m
+//  MissionHub
+//
+//  Created by Michael Harrison on 11/25/13.
+//  Copyright (c) 2013 Cru. All rights reserved.
+//
+
+#import "MHDirectionsActivity.h"
+
+@implementation MHDirectionsActivity
+
+@end
